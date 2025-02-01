4c081c79dad483c6fa23cb236e2453b504c8c8c3
andreyka101 <andrejkvasnin236@gmail.com>
2025-02-01 10:39:53 +0300

    effeefef

--- src/del/script.ts
+++ src/del/script.ts
@@ -1,19 +1,34 @@
+// document.querySelector("button")?.addEventListener("click",async ()=>{
+//     let data = await fetch("https://api.goodschool.online/payment_confirmation", {
+//         method: "POST",
+//         headers: {
+//             'Content-Type': 'application/json;charset=utf-8'
+//         },
+//         body: JSON.stringify({
+//             id:+(localStorage.getItem("id_student") as string),
+//             ok:true
+//         })
+//     }) as any
+//     data = await data.json()
+//     console.log(data);
+//     if(data.ok){
+//         localStorage.setItem("id_teacher",data.id_t)
+//         localStorage.setItem("id_student",data.id_s)
+//         window.location.href = "./personal_area_student.html"
+//     }
+// })
+
+
 document.querySelector("button")?.addEventListener("click",async ()=>{
-    let data = await fetch("https://api.goodschool.online/payment_confirmation", {
+    let data = await fetch("https://demo.bank131.ru/api/v1/session/create", {
         method: "POST",
         headers: {
-            'Content-Type': 'application/json;charset=utf-8'
+            'Content-Type': 'application/json',
+            "X-PARTNER-PROJECT": "vyglendalova_acq",
         },
-        body: JSON.stringify({
-            id:+(localStorage.getItem("id_student") as string),
-            ok:true
-        })
+        body: ""
     }) as any
+    console.log(data);
     data = await data.json()
     console.log(data);
-    if(data.ok){
-        localStorage.setItem("id_teacher",data.id_t)
-        localStorage.setItem("id_student",data.id_s)
-        window.location.href = "./personal_area_student.html"
-    }
 })
