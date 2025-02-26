2f3e2c0bd14073935366619e1c0222ac23f44add
andreyka101 <andrejkvasnin236@gmail.com>
2025-02-26 11:18:17 +0300

    wdwdwd

--- src/del/script.ts
+++ src/del/script.ts
@@ -1,5 +1,5 @@
 // document.querySelector("button")?.addEventListener("click",async ()=>{
-//     let data = await fetch("https://api.goodschool.online/payment_confirmation", {
+//     let data = await fetch("http://192.168.31.58:3000/payment_confirmation", {
 //         method: "POST",
 //         headers: {
 //             'Content-Type': 'application/json;charset=utf-8'
@@ -24,7 +24,7 @@
 
 
 document.querySelector("button")?.addEventListener("click", async () => {
-    let data = await fetch("https://api.goodschool.online/first_payment", {
+    let data = await fetch("http://192.168.31.58:3000/first_payment", {
         method: "POST",
         headers: {
             'Content-Type': 'application/json;charset=utf-8'
