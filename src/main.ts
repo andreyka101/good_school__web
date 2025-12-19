6c4c592d2eef0b98b35991e9b79611d017d88d81
andreyka101 <andrejkvasnin236@gmail.com>
2025-12-19 23:58:50 +0300

    кпкпкпкппк

--- src/main.ts
+++ src/main.ts
@@ -71,3 +71,64 @@ updateButtonVisibility();
 
 // Используем событие scroll для обновления состояния кнопок
 scrollableContent.addEventListener('scroll', updateButtonVisibility);
+
+
+
+
+// footer
+const phone = document.querySelector("#phone") as HTMLLinkElement
+const email = document.querySelector("#email") as HTMLLinkElement
+// @ts-ignore
+let detect = new MobileDetect(window.navigator.userAgent)
+
+if(detect.mobile() != null){
+    phone.href = "tel:+79189700187"
+    email.href = "mailto:goodschool80@mail.ru"
+}
+phone.addEventListener("click", () => {
+    if (detect.mobile() == null) {
+
+        navigator.clipboard.writeText("+79189700187").then(function () {
+            console.log('Текст успешно скопирован в буфер обмена');
+            phone.classList.add("button_active_1")
+            phone.innerHTML = `<div class="svg"></div>
+                <div class="information_text">
+                    номер скопирован
+                </div>`
+            setTimeout(() => {
+                phone.classList.remove("button_active_1")
+                phone.innerHTML = `<div class="svg"></div>
+            <div class="information_text">
+            +79189700187
+            </div>`
+            }, 3000)
+        }, function (err) {
+            console.error('Произошла ошибка при копировании текста: ', err);
+        });
+    }
+})
+
+email.addEventListener("click", () => {
+    if (detect.mobile() == null) {
+
+        navigator.clipboard.writeText("goodschool80@mail.ru").then(function () {
+            console.log('Текст успешно скопирован в буфер обмена');
+            email.classList.add("button_active_2")
+            email.innerHTML = `<div class="svg"></div>
+            <div class="information_text">
+            почта скопирована
+            </div>`
+            setTimeout(() => {
+                email.classList.remove("button_active_2")
+                email.innerHTML = `<div class="svg"></div>
+                <div class="information_text">
+                    goodschool80@mail.ru
+                </div>`
+            }, 3000)
+        }, function (err) {
+            console.error('Произошла ошибка при копировании текста: ', err);
+        });
+    }
+})
+//@ts-ignore
+console.log("Mobile: " + detect.mobile());
