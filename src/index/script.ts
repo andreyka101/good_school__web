509b1a0cf7b18898ecef795a735c12b4e35a52ae
andreyka101 <andrejkvasnin236@gmail.com>
2026-02-03 19:56:13 +0300

    0.9.7

--- src/index/script.ts
+++ src/index/script.ts
@@ -15,3 +15,7 @@ id_index_none2?.addEventListener("mouseover",()=>{
     localStorage.setItem("dataUser","")
     localStorage.setItem("registration_configuration","none")
 })
+// #00A287	#1E796A	#006957	#34D0B6	#5ED0BD
+// #104BA9	#284A7E	#052D6E	#447BD4	#6A93D4
+// #FFA200	#BF8B30	#A66A00	#FFBA40	#FFCC73
+// #FF6700	#BF6A30	#A64300	#FF8D40	#FFAB73
