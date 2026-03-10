a60e74a0e440601cc1c34cde1141cb6eb86ecf45
andreyka101 <andrejkvasnin236@gmail.com>
2026-03-10 09:09:52 +0300

    0.9.13

--- src/main.ts
+++ src/main.ts
@@ -78,63 +78,76 @@ scrollableContent.addEventListener('scroll', updateButtonVisibility);
 
 
 // footer
-const phone = document.querySelector("#phone_footer") as HTMLLinkElement
-const email = document.querySelector("#email_footer") as HTMLLinkElement
+// const phone = document.querySelector("#phone_footer") as HTMLLinkElement
+const phone = document.querySelectorAll(".link_phone") as NodeListOf<HTMLLinkElement>
+// const email = document.querySelector("#email_footer") as HTMLLinkElement
+const email = document.querySelectorAll(".link_email") as NodeListOf<HTMLLinkElement>
 console.log(1);
 console.log(phone);
 
 // @ts-ignore
 let detect = new MobileDetect(window.navigator.userAgent)
 
-if (detect.mobile() != null) {
-    phone.href = "tel:+79189700187"
-    email.href = "mailto:goodschool80@mail.ru"
+if (detect.mobile() == null) {
+    // phone.href = "tel:+79189700187"
+    // email.href = "mailto:goodschool80@mail.ru"
+    for (let el of phone) {
+        el.href = "javascript:void(0)"
+    }
+    for (let el of email) {
+        el.href = "javascript:void(0)"
+    }
 }
-phone.addEventListener("click", () => {
-    if (detect.mobile() == null) {
+for (let el of phone) {
+    el.addEventListener("click", () => {
+        if (detect.mobile() == null) {
 
-        navigator.clipboard.writeText("+79189700187").then(function () {
-            console.log('Текст успешно скопирован в буфер обмена');
-            phone.classList.add("button_active_1")
-            phone.innerHTML = `<div class="svg"></div>
+            navigator.clipboard.writeText("+79189700187").then(function () {
+                console.log('Текст успешно скопирован в буфер обмена');
+                el.classList.add("button_active_1")
+                el.innerHTML = `<div class="svg"></div>
                 <div class="information_text">
                     номер скопирован
                 </div>`
-            setTimeout(() => {
-                phone.classList.remove("button_active_1")
-                phone.innerHTML = `<div class="svg"></div>
+                setTimeout(() => {
+                    el.classList.remove("button_active_1")
+                    el.innerHTML = `<div class="svg"></div>
             <div class="information_text">
-            +79189700187
+                +79189700187
             </div>`
-            }, 3000)
-        }, function (err) {
-            console.error('Произошла ошибка при копировании текста: ', err);
-        });
-    }
-})
+                }, 3000)
+            }, function (err) {
+                console.error('Произошла ошибка при копировании текста: ', err);
+            });
+        }
+    })
+}
 
-email.addEventListener("click", () => {
-    if (detect.mobile() == null) {
-
-        navigator.clipboard.writeText("goodschool80@mail.ru").then(function () {
-            console.log('Текст успешно скопирован в буфер обмена');
-            email.classList.add("button_active_2")
-            email.innerHTML = `<div class="svg"></div>
+for (let el of email) {
+    el.addEventListener("click", () => {
+        if (detect.mobile() == null) {
+            navigator.clipboard.writeText("goodschool80@mail.ru").then(function () {
+                console.log('Текст успешно скопирован в буфер обмена');
+                el.classList.add("button_active_2")
+                el.innerHTML = `<div class="svg"></div>
             <div class="information_text">
             почта скопирована
             </div>`
-            setTimeout(() => {
-                email.classList.remove("button_active_2")
-                email.innerHTML = `<div class="svg"></div>
-                <div class="information_text">
-                    goodschool80@mail.ru
-                </div>`
-            }, 3000)
-        }, function (err) {
-            console.error('Произошла ошибка при копировании текста: ', err);
-        });
-    }
-})
+                setTimeout(() => {
+                    el.classList.remove("button_active_2")
+                    el.innerHTML = `
+                    <div class="svg"></div>
+                    <div class="information_text">
+                        goodschool80@mail.ru
+                    </div>
+                    `
+                }, 3000)
+            }, function (err) {
+                console.error('Произошла ошибка при копировании текста: ', err);
+            });
+        }
+    })
+}
 //@ts-ignore
 console.log("Mobile: " + detect.mobile());
 
